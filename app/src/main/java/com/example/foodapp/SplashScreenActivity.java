72aaac813c4d9a03f7f2f7b938fd49c0d3838cf3
Dipali <dipalikharat2798@gmail.com>
2021-05-13 15:12:49 +0530

    Splash Screen And Main Activity

---
+++ app/src/main/java/com/example/foodapp/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import static java.lang.Thread.sleep;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(3 * 1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+
+        };thread.start();
+
+    }
+}
